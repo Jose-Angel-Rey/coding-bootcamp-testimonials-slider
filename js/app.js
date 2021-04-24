7355c87409602606dae09991049ac87aa3c3fb06
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-04-24 14:31:54 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,36 +1,34 @@
-const testimonialAvatar = document.querySelector(".student-photo");
+let testimonialAvatar = document.querySelector(".student-photo");
 
-const testimonialContent = document.querySelector(
-  ".testimonial-content-container");
+let testimonialContent = document.querySelector(
+  ".testimonial-content-container"
+);
 
-const nextTestimonial = document.querySelector(".icon-next")
+let nextTestimonial = document.querySelector(".icon-next");
 
-const prevTestimonial = document.querySelector(".icon-prev")
+let prevTestimonial = document.querySelector(".icon-prev");
 
-nextTestimonial.addEventListener("click", () => {
+nextTestimonial.addEventListener("click", checkClass);
+prevTestimonial.addEventListener("click", checkClass);
+
+function checkClass() {
+  if (testimonialContent.children[0].classList.contains("hidden")) {
+    // Show Tanya testimonial
+    console.log(testimonialContent.children[0].classList.remove("hidden"));
+
+    // Hidde John testimonial
+    console.log(testimonialContent.children[1].classList.add("hidden"));
+
     // Change avatar image
-  testimonialAvatar.src = "../images/image-john.jpg";
+    testimonialAvatar.src = "../images/image-tanya.jpg";
+  } else {
+    // Show John testimonial
+    console.log(testimonialContent.children[1].classList.remove("hidden"));
 
-  // Change alt attribute from avatar image
-  testimonialAvatar.alt = "John Tarkpor";
+    // Hidde Tanya testimonial
+    console.log(testimonialContent.children[0].classList.add("hidden"));
 
-  // Hidde
-  console.log(testimonialContent.children[0].classList.add("hidden"));
-
-  // Show
-  console.log(testimonialContent.children[1].classList.remove("hidden"));
-  })
-
-prevTestimonial.addEventListener("click", () => {
     // Change avatar image
-  testimonialAvatar.src = "../images/image-tanya.jpg";
-
-  // Change alt attribute from avatar image
-  testimonialAvatar.alt = "Tanya Sinclair";
-
-  // Hidde
-  console.log(testimonialContent.children[1].classList.add("hidden"));
-
-  // Show
-  console.log(testimonialContent.children[0].classList.remove("hidden"));
-  })
+    testimonialAvatar.src = "../images/image-john.jpg";
+  }
+}
